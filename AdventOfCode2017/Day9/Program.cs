10046f87227fffdb7f32c5ee0bf68c2746a52219
Jon Wahlström <jon.wahlstrom@afconsult.com>
2017-12-09 11:34:47 +0100

    Add Day 9

---
+++ AdventOfCode2017/Day9/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+
+namespace Day9
+{
+	public static class Extensions
+	{
+		public static int[] ReadStream(this string stream)
+		{
+			int indent = 0, score = 0, garbageCount = 0;
+			bool ignore = false, garbage = false;
+
+			foreach (var c in stream)
+			{
+				if (ignore)
+				{
+					ignore = false;
+					continue;
+				}
+
+				if (garbage)
+				{
+					switch (c)
+					{
+						case '!':
+							ignore = true;
+							continue;
+						case '>':
+							garbage = false;
+							continue;
+						default:
+							garbageCount++;
+							continue;
+					}
+				}
+				else
+				{
+					switch (c)
+					{
+						case '<':
+							garbage = true;
+							continue;
+						case '{':
+							indent++;
+							continue;
+						case '}':
+							score += indent;
+							indent--;
+							continue;
+					}
+				}
+			}
+
+			return new [] { score, garbageCount };
+		}
+	}
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var input = File.ReadAllText("../../input.txt");
+			var result = input.ReadStream();
+			Console.WriteLine($"Part One: {result[0]}");
+			Console.WriteLine($"Part Two: {result[1]}");
+			Console.Read();
+		}
+	}
+}
