97a572bfc87bb7f59574e781634b9e6f892e4028
Jon Wahlström <jon.wahlstrom@afconsult.com>
2017-12-10 15:27:18 +0100

    Add Day 10

---
+++ AdventOfCode2017/Day10/Extensions.cs
@@ -0,0 +1,69 @@
+﻿using System.Linq;
+using System.Text;
+
+namespace Day10
+{
+	public static class Extensions
+	{
+		private static void Reverse(this byte[] A, int index, int count)
+		{
+			for (int i = 0; i < count / 2; ++i)
+			{
+				int a = (index + i) % A.Length;
+				int b = (index + ((count - 1) - i)) % A.Length;
+				
+				var tmp = A[a];
+				A[a] = A[b];
+				A[b] = tmp;
+			}
+		}
+
+		private static byte[] ToKnotHash(this byte[] input, int rounds = 1)
+		{
+			byte length;
+			int currentPos = 0, skipSize = 0;
+			var result = Enumerable.Range(0, 256).Select(i => (byte)i).ToArray();
+
+			for (int i = 0; i < rounds; ++i)
+			{
+				for (int j = 0; j < input.Length; ++j)
+				{
+					length = input[j];
+					if (length <= result.Length)
+					{
+						result.Reverse(currentPos, length);
+						currentPos = (currentPos + (length + skipSize)) % result.Length;
+						skipSize++;
+					}
+				}
+			}
+
+			return result;
+		}
+
+		public static byte[] ToKnotHash(this string input)
+		{
+			var inputArray = input.Split(',').Select(s => byte.Parse(s)).ToArray();
+			return inputArray.ToKnotHash();
+		}
+
+		public static string ToKnotHash64(this string input)
+		{
+			var inputArray = ASCIIEncoding.ASCII.GetBytes(input)
+				.Concat(new byte[] { 17, 31, 73, 47, 23 })
+				.ToArray();
+
+			var denseHash = new long[16];
+			var sparseHash = inputArray.ToKnotHash(64);
+			for (int i = 0, j = 0; i < sparseHash.Length; i += 16, ++j)
+			{
+				for (int k = 0; k < 16; ++k)
+				{
+					denseHash[j] ^= sparseHash[i + k];
+				}
+			}
+
+			return string.Join("", denseHash.ToList().ConvertAll(i => string.Format("{0:x2}", i)));
+		}
+	}
+}
---
+++ AdventOfCode2017/Day10/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Day10
+{
+	public interface ISolution
+	{
+		void Test();
+		void Solve(string input);
+	}
+
+	public class PartOne : ISolution
+	{
+		public void Solve(string input)
+		{
+			var result = input.ToKnotHash();
+			Console.WriteLine($"Part One: {result[0] * result[1]}");
+		}
+
+		public void Test() { }
+	}
+
+	public class PartTwo : ISolution
+	{
+		public void Solve(string input)
+		{
+			var result = input.ToKnotHash64();
+			Console.WriteLine($"Part Two: {result}");
+		}
+
+		public void Test()
+		{
+			var inputs = new[] { "", "AoC 2017", "1,2,3", "1,2,4" };
+			var answers = new[] { "a2582a3a0e66e6e86e3812dcb672a272", "33efeb34ea91902bb2f59c9920caa6cd", "3efbe78a8d82f29979031a4aa0b16a9d", "63960835bcdc130f0b66d7ff4f6a5a8e" };
+			for (int i = 0; i < inputs.Length; ++i)
+			{
+				if (inputs[i].ToKnotHash64() != answers[i])
+				{
+					throw new Exception();
+				}
+			}
+		}
+	}
+
+	class Program
+	{
+		static void Run<T>(string input) where T : ISolution, new()
+		{
+			var t = new T();
+			t.Test();
+			t.Solve(input);
+		}
+
+		static void Main(string[] args)
+		{
+			var input = "63,144,180,149,1,255,167,84,125,65,188,0,2,254,229,24";
+			Run<PartOne>(input);
+			Run<PartTwo>(input);
+			Console.Read();
+		}
+	}
+}
